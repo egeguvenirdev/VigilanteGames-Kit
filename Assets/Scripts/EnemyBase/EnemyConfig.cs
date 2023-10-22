14d44b36baf2df91a416f71a9b7bc72831c911ab
Ege Güvenir <egeguvenirdev@gmail.com>
2023-10-22 16:16:08 +0300

    Enemy Config

---
+++ Assets/Scripts/EnemyBase/EnemyConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public readonly struct EnemyConfig
+{
+    public readonly float Size;
+    public readonly float Power;
+    public readonly int Money;
+
+    public EnemyConfig(float size, float power, int money)
+    {
+        Size = size;
+        Power = power;
+        Money = money;
+    }
+}
---
+++ Assets/Scripts/EnemyBase/EnemyConfigUtility.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public static class EnemyConfigUtility
+{
+    //Dictionary
+    private static readonly Dictionary<byte, EnemyConfig> enemyConfigByLevel = new Dictionary<byte, EnemyConfig>()
+    {
+        {
+            1,
+            new EnemyConfig(3, 35, 500)
+        },
+        {
+            2,
+            new EnemyConfig(4, 45, 1000)
+        }
+    };
+
+    private static readonly EnemyConfig defaultConfig = new EnemyConfig(3, 35, 500);
+
+    public static EnemyConfig GetEnemyConfigByLevel(byte level)
+    {
+        if (enemyConfigByLevel.TryGetValue(level, out var enemyConfig)) return enemyConfig;
+        Debug.LogWarning("There is no config by given enemy level, default config returned!");
+        return defaultConfig;
+    }
+}

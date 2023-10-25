36a7f0cff254202003d6ccebd892f72cd49bde9b
Ege Güvenir <egeguvenirdev@gmail.com>
2023-10-25 14:22:07 +0300

    cam follower

--- Assets/Scripts/Cam/CamFollower.cs
+++ Assets/Scripts/Cam/CamFollower.cs
@@ -5,25 +5,25 @@ using DG.Tweening;
 
 public class CamFollower : MonoBehaviour
 {
-    [SerializeField] private Transform player;
-
-    [SerializeField] private Vector3 offset;
+    [Header("Cam Follow Settings")]
+    [SerializeField] private Vector3 followOffset;
     [SerializeField] private float playerFollowSpeed = 0.125f;
-    //[SerializeField] private float clampLocalX = 1.5f;
+    [SerializeField] private float clampLocalX = 1.5f;
+
+    private Transform player;
 
     public void Init()
     {
         DOTween.Init();
-        player = PlayerManager.Instance.transform;
+        player = PlayerManager.Instance.GetCharacterTransform;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (player != null)
         {
-            Vector3 targetPosition = player.position + offset;
-            //targetPosition.x = Mathf.Clamp(targetPosition.x, -clampLocalX, clampLocalX);
+            Vector3 targetPosition = player.position + followOffset;
+            targetPosition.x = Mathf.Clamp(targetPosition.x, -clampLocalX, clampLocalX);
             transform.position = Vector3.Lerp(transform.position, targetPosition, playerFollowSpeed);
         }
     }

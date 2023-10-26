f922fa34fa0861e1124f743d909a43ddd11f965d
Ege Güvenir <egeguvenirdev@gmail.com>
2023-10-26 15:49:16 +0300

    new upgrade buttons

---
+++ Assets/Scripts/UI/Buttons/UpgradeCards/FireRangeUpgrade.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireRangeUpgrade : UpgradeCard
+{
+    public override void OnButtonClick()
+    {
+        base.OnButtonClick();
+        //
+    }
+}
---
+++ Assets/Scripts/UI/Buttons/UpgradeCards/IncomeUpgrade.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncomeUpgrade : UpgradeCard
+{
+    public override void OnButtonClick()
+    {
+        base.OnButtonClick();
+        //
+    }
+}

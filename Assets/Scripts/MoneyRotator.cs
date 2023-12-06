d012351982a086e818e8a9baef244b00b5c2a325
Ege Güvenir <egeguvenirdev@gmail.com>
2023-12-06 19:08:38 +0300

    Money Rotator fix

--- Assets/Scripts/MoneyRotator.cs
+++ Assets/Scripts/MoneyRotator.cs
@@ -7,22 +7,30 @@ public class MoneyRotator : MonoBehaviour
     [SerializeField] private Vector3 _rotateVelocity;
     [SerializeField] private Space _rotateSpace;
     [SerializeField] private float speed = 4f;
+    [SerializeField] private float additionalHeight = 0.5f;
 
     private Vector3 minHeight;
     private Vector3 maxHeight;
 
-    private void Start()
+    public void Init()
     {
         minHeight = transform.position;
-        maxHeight = minHeight + new Vector3(0, 0.5f, 0);
+        maxHeight = minHeight + new Vector3(0, additionalHeight, 0);
+
+        ActionManager.UpdateManager += OnUpdate;
     }
 
-    void Update()
+    public void DeInit()
+    {
+        ActionManager.UpdateManager -= OnUpdate;
+    }
+
+    private void OnUpdate(float deltaTime)
     {
         transform.Rotate(_rotateVelocity * Time.deltaTime, _rotateSpace);
 
         float lerpValue = (Mathf.Sin(speed * Time.time) + 1f) / 2f; // <0, 1> 
-        transform.position = Vector3.Lerp(minHeight, maxHeight, lerpValue);
+        transform.position = Vector3.Lerp(minHeight, maxHeight, lerpValue * deltaTime);
     }
 }
 

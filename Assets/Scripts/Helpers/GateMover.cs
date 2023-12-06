913e207b3f215a8c9b0f782890862953f8944c2d
Ege Güvenir <egeguvenirdev@gmail.com>
2023-12-06 19:15:46 +0300

    gatemoverfix

--- Assets/Scripts/Helpers/GateMover.cs
+++ Assets/Scripts/Helpers/GateMover.cs
@@ -7,14 +7,14 @@ public class GateMover : MonoBehaviour
 {
     private Vector3 swerveVector;
     [SerializeField] private float swerveFloat = -1.2f;
-    // Start is called before the first frame update
+
     void Start()
     {
         DOTween.Init();
         StartMovement();
     }
 
-    // Update is called once per frame
+
     private void StartMovement()
     {
         swerveVector = new Vector3(swerveFloat, transform.position.y, transform.position.z);

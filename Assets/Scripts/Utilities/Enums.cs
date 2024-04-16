7ebdd0afa9b6b36eaaf8772a0bdae571653d28b3
Ege Güvenir <egeguvenirdev@gmail.com>
2024-04-17 02:54:52 +0300

    Enums are seperated

---
+++ Assets/Scripts/Utilities/Enums.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UpgradeType
+{
+    Income,
+    FireRate,
+    FireRange
+}
+
+public enum EnemyType
+{
+    Melee,
+    Range
+}
+
+public enum PoolObjectType
+{
+    PlayerThrowable,
+    PlayerGravityThrowable,
+    RangeEnemy,
+    MeeleEnemy,
+    EnemyThrowable,
+    SlideText,
+    Coin,
+    Health,
+    BloodParticle
+}
+
+[System.Flags]
+public enum DropType : int
+{
+    Nothing = 0x00,
+    Coin = 0x01,
+    Health = 0x02,
+    Armor = 0x04
+}
+
+public class Enums : MonoBehaviour
+{
+    //
+}

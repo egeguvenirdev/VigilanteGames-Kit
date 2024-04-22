8c77ac0328b1146293fcf24b00dc3b6ddb7ca0a2
Ege Güvenir <egeguvenirdev@gmail.com>
2024-04-22 04:47:27 +0300

    Income Upgrade Fix

--- Assets/Scripts/Upgrade System/IncomeUpgrade.cs
+++ Assets/Scripts/Upgrade System/IncomeUpgrade.cs
@@ -7,6 +7,5 @@ public class IncomeUpgrade : UpgradeBase
     public override void OnUpgrade(float upgradeValue)
     {
         base.OnUpgrade(upgradeValue);
-        ActionManager.GameplayUpgrade?.Invoke(UpgradeType.Income, upgradeInfos.GetUpgradeInfos.CurrentValue);
     }
 }
